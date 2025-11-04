bd3f6c5eb5b93d5175452409801b7ac24c4aef14
chris aylott <chris.aylott@gmail.com>
2025-11-04 10:43:53 +0000

    clean

--- devcontainer/scripts/utils.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-log_info() { echo -e "\033[1;34m[INFO]\033[0m $*"; }
-log_success() { echo -e "\033[1;32m[SUCCESS]\033[0m $*"; }
-log_error() { echo -e "\033[1;31m[ERROR]\033[0m $*"; }
